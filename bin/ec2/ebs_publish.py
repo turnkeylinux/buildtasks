932180dc4f183e316230bb01f928dfe440d4a3fc
Alon Swartz <alon@turnkeylinux.org>
2013-04-24 16:19:11 +0300

    added ebs_publish

---
+++ bin/ec2/ebs_publish.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# Copyright (c) 2013 Alon Swartz <alon@turnkeylinux.org>
+"""
+Set AMI launch permission to all
+
+Arguments:
+
+    ami_id          Amazon Image ID
+
+Options:
+
+    --region=       Region (default: current region)
+
+"""
+import sys
+import getopt
+
+import utils
+
+def usage(e=None):
+    if e:
+        print >> sys.stderr, "error: " + str(e)
+
+    print >> sys.stderr, "Syntax: %s [ -options ] ami_id" % (sys.argv[0])
+    print >> sys.stderr, __doc__.strip()
+
+    sys.exit(1)
+
+def share_public(ami_id, region):
+    conn = utils.connect(region)
+
+    conn.modify_image_attribute(
+        ami_id,
+        attribute='launchPermission',
+        operation='add',
+        groups=['all'])
+
+def main():
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "h", ["help", "region="])
+    except getopt.GetoptError, e:
+        usage(e)
+
+    region = None
+    for opt, val in opts:
+        if opt in ('-h', '--help'):
+            usage()
+
+        if opt == "--region":
+            region = val
+
+    if len(args) != 1:
+        usage("incorrect number of arguments")
+
+    ami_id = args[0]
+    region = region if region else utils.get_region()
+
+    share_public(ami_id, region)
+
+if __name__ == "__main__":
+    main()
+
