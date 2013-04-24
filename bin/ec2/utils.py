5ac03d72278a1461a8bda6131507c0ddfae962b5
Alon Swartz <alon@turnkeylinux.org>
2013-04-24 16:22:56 +0300

    added ec2_copy

--- bin/ec2/utils.py
+++ bin/ec2/utils.py
@@ -28,6 +28,9 @@ def get_zone():
 def get_region():
     return ec2metadata.get('availability-zone')[0:-1]
 
+def get_regions():
+    return conf.KERNELS.keys()
+
 def get_kernel(region, arch):
     return conf.KERNELS[region][arch]
 
